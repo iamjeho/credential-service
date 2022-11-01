700a8d326b87f86fe3ad801d54a05c3be6893c5a
jeho <jiho0726@junhyuk-virtualbox.localdomain>
2022-11-01 15:31:09 +0900

    The First Commit

---
+++ web-app2/qr_app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  fs.readFile('qr_public/index.html', 'utf-8', (err, data) => {
+    if(err) { res.send('No Such File of Directory') }
+    res.send(data)
+  })
+})
+
+app.get('/qrcode', (req, res) => {
+  fs.readFile('public/qrcode.png', (err, data) => {
+    if(err) { res.send() }
+    res.send(data)
+  })
+})
+
+app.listen(5050)
